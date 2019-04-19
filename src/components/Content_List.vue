6495063e53246e4be823510d9a47a407ee607d15
hxj <1072700686@qq.com>
2019-04-19 23:28:47 +0800

    2nd version, spilit the Content_List component in to Item_List component, it's ok.

--- src/components/Content_List.vue
+++ src/components/Content_List.vue
@@ -1,30 +1,17 @@
 <template>
     <div id="content">
         <p v-show="!lists.length"><i>暂无待办事项</i></p>
-        <ul>
-            <li v-for="(list,index) in lists" :key="index">
-                <input type="checkbox" :id="index" :value="index" v-model="selectedList">
-                <label :for="index"  @click="listCheck()">
-                    <span>{{index+1}}. {{list}}  {{selectedList.length}}</span>
-                </label>
-            </li>
-        </ul>
+        <Item_List :lists="lists" :add-tmp="addTmp"></Item_List>
     </div>
 </template>
 
 <script>
+    import Item_List from './Item_List'
   export default {
     name: 'Content_List',
       props:['lists','addTmp'],
-      data(){
-        return{
-            selectedList:[]
-        }
-      },
-      methods:{
-        listCheck(){
-
-        }
+      components:{
+          Item_List,
       }
   }
 </script>
@@ -36,32 +23,6 @@
         margin: 10px 0;
         border: solid 1px green;
     }
-
-    #content ul {
-        position: relative;
-        margin: 5px;
-        width: 100%;
-    }
-
-    #content ul li{
-        position: relative;
-        width: 100%;
-        height: 25px;
-        display: block;
-    }
-
-    #content ul li span {
-        position: relative;
-        width: 90%;
-        padding-left: 7px;
-        border-radius: 7px;
-        display: inline-block;
-    }
-
-    #content ul li span:hover{
-        background-color: #eeeeee;
-    }
-
     #content p{
         margin: 20px;
         text-align: center;
---
+++ src/components/Item_List.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(list,index) in lists" :key="index">
+                <input type="checkbox" :id="index" :value="index" v-model="selectedList">
+                <label :for="index"  @click="listCheck()">
+                    <span>{{index+1}}. {{list}}  {{selectedList.length}}</span>
+                    <button>删除</button>
+                </label>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'Item_List',
+      props:['lists','addTmp'],
+      data(){
+          return{
+              selectedList:[]
+          }
+      },
+      methods:{
+          listCheck(){
+
+          }
+      }
+  }
+</script>
+
+<style scoped>
+    ul {
+        position: relative;
+        margin: 5px;
+        width: 100%;
+    }
+
+    ul li{
+        position: relative;
+        width: 100%;
+        height: 25px;
+        display: block;
+    }
+
+    ul li span {
+        position: relative;
+        width: 90%;
+        padding-left: 7px;
+        border-radius: 7px;
+        display: inline-block;
+    }
+
+    ul li span:hover{
+        background-color: #eeeeee;
+    }
+
+    ul li button{
+        position: absolute;
+        right: 20px;
+        display: none;
+    }
+
+</style>
